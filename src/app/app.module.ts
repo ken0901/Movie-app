c9fa3a46dedcb0dcce62c01e3d12a19e64b01a83
ken0901 <ken5260715@gmail.com>
2023-05-11 20:43:51 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        
+    ],
+    imports: [
+        BrowserModule,
+       
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
