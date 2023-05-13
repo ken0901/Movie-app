58936feb0ea64de55bd7f02a95ee72a9110885e0
ken0901 <ken5260715@gmail.com>
2023-05-12 22:30:27 -0700

    Service for getting popular movies

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'app/services/movies.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  movies: any = [];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) { }
 
   ngOnInit(): void {
+    this.moviesService.getMovies()
+      .subscribe(
+        (data: any) => {
+          this.movies = data.results;
+          console.log(this.movies);
+        }
+    );
   }
 
 }
