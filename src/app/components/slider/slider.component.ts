a5e9e80b7fc933459b9b756ca4736a43fec5eb74
ken0901 <ken5260715@gmail.com>
2023-05-16 22:12:01 -0700

    Slider Animation and set timer

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,17 +1,27 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'app/models/movie.model';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.css'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({opacity: 0})),
+      transition('void <=> *', [animate('1s')]),
+    ]) 
+  ]
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
 
-  constructor() { }
+  currentSlideIndex : number = 0;
 
   ngOnInit(): void {
+    setInterval( () => {
+      this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+    }, 5000)
   }
 
 }
