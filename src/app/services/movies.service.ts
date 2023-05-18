f0d411d1197e640471bd793a5cb2a287baf75632
ken0901 <ken5260715@gmail.com>
2023-05-17 22:27:30 -0700

    Adjust the services to return number of movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { environment } from 'environments/environment';
+import { MovieDto } from '../models/movie.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,10 @@ export class MoviesService {
 
   constructor(private http: HttpClient) { }
 
-  getMovies(type: string = 'upcoming'): Observable<any>{
-    return this.http.get(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`)
+  getMovies(type: string = 'upcoming', count: number = 12) {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`)
+      .pipe(switchMap( res => {
+        return of(res.results.slice(0, count))
+      }));
   }
 }
